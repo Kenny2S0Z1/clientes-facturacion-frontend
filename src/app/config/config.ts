5ca5600ffbabf7bfcdd1fb3c870b3fce2e4c0b01
Kenny2S0Z1 <silvazambranokennyandres@gmail.com>
2025-09-18 12:27:18 -0500

    'urls_changed'

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,4 @@
-export const URL_BACKEND='https://spring-boot2-heroku-rest-4239d6492500.herokuapp.com';
-export const REDIRECT_URL='https://clientes-app-997af.web.app/login/oauth2/code/angularapp'
+
+export const URL_BACKEND='';//TU BACKEND
+export const REDIRECT_URL=''//TU URL DE REDIRECCIÓN DE BACKEND A FRONTEND
+
